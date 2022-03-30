a8807ca504efc573aa0ff6611164111a8de9e267
smallGod <davies.mugume@gmail.com>
2022-03-30 17:13:23 +0300

    bump jakarta version to 9 for v7.0.1

--- integration/admin-client/src/main/java/org/keycloak/admin/client/resource/AttackDetectionResource.java
+++ integration/admin-client/src/main/java/org/keycloak/admin/client/resource/AttackDetectionResource.java
@@ -17,14 +17,13 @@
 
 package org.keycloak.admin.client.resource;
 
-import org.jboss.resteasy.annotations.cache.NoCache;
-
 import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+
 import java.util.Map;
 
 /**
